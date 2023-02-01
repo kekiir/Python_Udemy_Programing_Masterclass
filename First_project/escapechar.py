74c2f1be99a273c4953eb5ee4ca2b8d3cf650597
Kéki Szilveszter <kekiir@gmail.com>
2023-02-01 15:59:54 +0100

    using \ in print function

--- First_project/escapechar.py
+++ First_project/escapechar.py
@@ -1,20 +1,30 @@
-
 splitString = "This string has been \nsplit over\nsevereal\nlines"
 print(splitString)
 
-tabbedString="1\t2\t3\t4\t5"
+tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
-#or "
+# or "
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 
-print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
-print(6*6)
+print("""The pet shop owner said "No, no, \ 
+ 'e's uh,...he's resting".""")
+print(6 * 6)
 
-anotherSplitString = """This string has been
-split over
-severeal
+anotherSplitString = """This string has been 
+split over 
+severeal 
+lines"""
+
+anotherSplitStringwithOutEnters = """This string has been \
+split over \
+severeal \
 lines"""
 
 print(anotherSplitString)
+print(anotherSplitStringwithOutEnters)
+
+#print("C:\users\timbuchalka\notes.txt")
+print(r"C:\users\timbuchalka\notes.txt")
+print("C:\\users\\timbuchalka\\notes.txt")
